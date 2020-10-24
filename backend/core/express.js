c7c22005ef57371d2b41b47a98432af382aa41e0
MIKE432 <michal.raszczuk.apusoft@gmail.com>
2020-10-24 19:28:22 +0200

    .

--- backend/core/express.js
+++ backend/core/express.js
@@ -1,10 +1,16 @@
 const express = require('express'),
-      PORT = 8080;
+      PORT = 8080,
+      defaults = require('../defaults/defaults')
 
 
 exports.initExpress = () => {
     let app = express();
 
+    const routes = glob.sync(path.resolve(defaults.patterns.routes));
+
+    routes.forEach((route) => {
+        require(route)(app);
+    });
 
     app.route('/*').get((req, res) => {
         return res.send({hey: "hey"})
