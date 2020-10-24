59ec86c6a2e3826f19d8259e2b9ecfd6ba3c02cd
damian <dzoltwoski@oakfusion.pl>
2020-10-25 00:20:50 +0200

    added Foreign key to db

--- backend/sql/createDb.sql
+++ backend/sql/createDb.sql
@@ -10,13 +10,16 @@ CREATE TABLE Petitions (
     title       CHAR(50) NOT NULL,
     descript    TEXT NOT NULL,
     likeCount   INT NOT NULL,
-    CONSTRAINT fk_users FOREIGN KEY (id) REFERENCES Users(id)
+    userID      INT NOT NULL
 );
 
 INSERT INTO Users VALUES (1, 'Dejmi', 'Damian', 'Zoltowski');
 INSERT INTO Users VALUES (2, 'Kubi', 'Jakub', 'Smolka');
 INSERT INTO Users VALUES (3, 'Chuj', 'Michal', 'Raszczuk');
 
-INSERT INTO Petitions VALUES (1, 'Petition1', 'No to jest petycja 1', 0);
-INSERT INTO Petitions VALUES (2, 'Petition2', 'No to jest petycja 2', 5);
-INSERT INTO Petitions VALUES (3, 'Petition3', 'No to jest petycja 3', 12);
+INSERT INTO Petitions VALUES (1, 'Petition1', 'No to jest petycja 1', 0, 2);
+INSERT INTO Petitions VALUES (2, 'Petition2', 'No to jest petycja 2', 5, 3);
+INSERT INTO Petitions VALUES (3, 'Petition3', 'No to jest petycja 3', 12, 1);
+
+ALTER TABLE ONLY Petitions 
+    ADD CONSTRAINT petitions_fk FOREIGN KEY (userID) REFERENCES Users(id);
