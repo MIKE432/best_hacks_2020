d4a83ae86ade42b816d15837f4d8e35f2d9e0ee5
damian <dzoltwoski@oakfusion.pl>
2020-10-24 23:18:44 +0200

    adding new models

--- backend/sql/createDb.sql
+++ backend/sql/createDb.sql
@@ -11,3 +11,11 @@ CREATE TABLE Petitions (
     descript    TEXT NOT NULL,
     CONSTRAINT fk_users FOREIGN KEY (id) REFERENCES Users(id)
 );
+
+INSERT INTO Users VALUES (1, "Dejmi", "Damian", "Zoltowski")
+INSERT INTO Users VALUES (2, "Kubi", "Jakub", "Smolka")
+INSERT INTO Users VALUES (3, "Chuj", "Michal", "Raszczuk")
+
+INSERT INTO Petitions VALUES (1, "Petition1", "No to jest petycja 1", 2)
+INSERT INTO Petitions VALUES (2, "Petition2", "No to jest petycja 2", 1)
+INSERT INTO Petitions VALUES (3, "Petition3", "No to jest petycja 3", 3)
