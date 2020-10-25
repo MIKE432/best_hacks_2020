3e0080ca1d429835d8dcab6a49f6af916f60c6d9
damian <dzoltwoski@oakfusion.pl>
2020-10-25 07:15:11 +0100

    added amazing descriptions

--- backend/sql/createDb.sql
+++ backend/sql/createDb.sql
@@ -22,5 +22,5 @@ INSERT INTO Petitions(title, descript, likeCount, petitor) VALUES ('Petycja o za
                                     'Pojazdy spalinowe produkują ok. 900g CO2 na każdy przejechany kilometr. Według badań z 2016 roku emisje z transportu stanowiły 25% całkowitych emisji CO2 
                                     w Unii Europejskiej. Zmiana pojazdów spalinowych na elektryczne pozwoliłaby na znaczne zmniejszenie emisji gazu cieplarnianego, co 
                                     przyczyniłoby się na zdrowszą atmosferę oraz dłuższe i bardziej komfortowe życie człowieka na naszej planecie.',
-                                    12,
+                                    119,
                                     'Damian Zoltowski');
