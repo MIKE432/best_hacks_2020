6603b378e13118805a7521986712f031da53d792
damian <dzoltwoski@oakfusion.pl>
2020-10-25 05:22:03 +0100

    fixed autoincrement for db data

--- backend/sql/createDb.sql
+++ backend/sql/createDb.sql
@@ -1,11 +1,11 @@
-CREATE TABLE Petitions(
-    petitionID      SERIAL PRIMARY KEY NOT NULL,
+CREATE TABLE Petitions (
+    petitionID      SERIAL PRIMARY KEY,
     title           TEXT NOT NULL,
     descript        TEXT NOT NULL,
     likeCount       INT NOT NULL,
     petitor         TEXT NOT NULL
 );
 
-INSERT INTO Petitions VALUES (1, 'Petition1', 'No to jest petycja 1', 0, 'Jakub Smolka');
-INSERT INTO Petitions VALUES (2, 'Petition2', 'No to jest petycja 2', 5, 'Michal Raszczuk');
-INSERT INTO Petitions VALUES (3, 'Petition3', 'No to jest petycja 3', 12, 'Damian Zoltowski');
+INSERT INTO Petitions(title, descript, likeCount, petitor) VALUES ('Petition1', 'No to jest petycja 1', 0, 'Jakub Smolka');
+INSERT INTO Petitions(title, descript, likeCount, petitor) VALUES ( 'Petition2', 'No to jest petycja 2', 5, 'Michal Raszczuk');
+INSERT INTO Petitions(title, descript, likeCount, petitor) VALUES ( 'Petition3', 'No to jest petycja 3', 12, 'Damian Zoltowski');
